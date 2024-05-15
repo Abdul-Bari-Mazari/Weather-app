46315b3c0f4b4acdb197aa4f9b71f09adaae375e
Abdul-Bari-Mazari <mazari3339@gmail.com>
2024-05-16 00:46:00 +0500

    Added current location weather on app launch.
    -On app launch, user will be asked for location access. If given then his current location weather will be shown.
    
    If denied access then weather of Pakistan will be shown

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const body = document.getElementById("body"),
+  weatherIcon = document.getElementById("weatherIcon"),
   searchBar = document.getElementById("searchBar"),
   cityName = document.getElementById("cityName"),
   // temperature = document.getElementById("temp"),
@@ -10,52 +11,99 @@ const body = document.getElementById("body"),
   windSpeed = document.getElementById("windSpeed"),
   visibility = document.getElementById("visibility");
 
-//   var lat, lon;
-// function getCurrentLocation() {
-//  var location = new Promise navigator.geolocation.getCurrentPosition(function (location) {
-//     lat = location.coords.latitude;
-//     lon = location.coords.longitude;
-//     console.log(lat, lon);
-//   });
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
-//   console.log(lat, lon);
-//   fetch(
-//     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6be255ff5e9fc9c77fa08bd4064fc2a8&units=metric`
-//   )
-//     .then(function (res) {
-//       return res.json();
-//     })
-//     .then(function (res) {
-//       response(res);
-//       data = res;
-//       console.log(data);
-//     })
-//     .catch(function (err) {
-//       err(res);
-//     });
-// }
+var lat, lon;
 
-// function fetchData() {
-//   return new Promise(function (resolve, reject) {
-//     fetch(
-//       `https://api.openweathermap.org/data/2.5/weather?q=${searchBar.value
-//         .toLowerCase()
-//         .trim()}&appid=6be255ff5e9fc9c77fa08bd4064fc2a8&units=metric`
-//     )
-//       .then(function (res) {
-//         return res.json();
-//       })
-//       .then(function (res) {
-//         resolve(res);
+function getCurrentLocation(callBack, errorCallback) {
+  navigator.geolocation.getCurrentPosition(
+    function (location) {
+      const lat = location.coords.latitude;
+      const lon = location.coords.longitude;
+      callBack(lat, lon);
+    },
+    function (error) {
+      errorCallback(error);
+    }
+  );
+}
 
-//       })
-//       .catch(function (res) {
-//         reject(res);
-//       });
-//   });
-// }
+function callBack(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=6be255ff5e9fc9c77fa08bd4064fc2a8&units=metric`
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (res) {
+      console.log(lat, lon);
+      setCurrentWeather(res);
+    })
+    .catch(function (err) {
+      console.log("catch", err);
+    });
+}
 
-// getCurrentLocation();
+function setCurrentWeather(data) {
+  console.log(data);
+  cityName.innerText = data.name;
+  var temp = `${data.main.temp.toFixed(1)}&deg;C`;
+  var weather = data.weather[0].main;
+  var condition = capitalize(data.weather[0].description);
+  weatherCondition.innerHTML = `${condition} - ${temp}`;
+  // var max = `${data.main.temp_max}&deg;`;
+  // var min = `${data.main.temp_min}&deg;`;
+  // max_minHTML.innerHTML = `${max}/${min}`;
+  feelsLike.innerHTML = `${data.main.feels_like}&deg;C`;
+  humidity.innerHTML = `${data.main.humidity}%`;
+  windSpeed.innerHTML = `${data.wind.speed}km/h`;
+  visibility.innerHTML = `${(data.visibility / 1000).toFixed(2)}km`;
+
+  if (weather === "Clear" || weather === "clear sky") {
+    body.style.backgroundImage = "url(./imgs/background/clear.jpg)";
+    weatherIcon.src = "./imgs/icons/sun.png";
+  } else if (
+    weather === "few clouds" ||
+    weather === "Clouds" ||
+    weather === "overcast clouds" ||
+    weather === "scattered clouds" ||
+    weather === "broken clouds"
+  ) {
+    body.style.backgroundImage = "url(./imgs/background/cloudy.jpg)";
+    weatherIcon.src = "./imgs/icons/cloudy.png";
+  } else if (weather === "Rain" || weather === "light rain") {
+    body.style.backgroundImage = "url(./imgs/background/rainy.jpg)";
+    weatherIcon.src = "./imgs/icons/rainy.png";
+  } else if (weather === "Haze" || weather === "Smoke") {
+    body.style.backgroundImage = "url(./imgs/background/haze.jpg)";
+  } else if (weather === "Snow") {
+    body.style.backgroundImage = "url(./imgs/background/snow.webp)";
+  }
+}
+
+function errorCallback(error) {
+  console.log(`Error getting location: ${error.message}`);
+  setPakistaWeather();
+}
+
+function setPakistaWeather() {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=Pakistan&appid=6be255ff5e9fc9c77fa08bd4064fc2a8&units=metric`
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (res) {
+      setCurrentWeather(res);
+    })
+    .catch(function (err) {
+      console.log("catch", err);
+    });
+}
+
+getCurrentLocation(callBack, errorCallback);
 
 function search() {
   return new Promise(function (resolve, reject) {
@@ -78,15 +126,11 @@ function search() {
   });
 }
 
-function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
 function displayData(data) {
   console.log("data", data);
   cityName.innerText = data.name;
   var temp = `${data.main.temp.toFixed(1)}&deg;C`;
-  var weather = data.weather[0].description;
+  var weather = data.weather[0].main;
   var condition = capitalize(data.weather[0].description);
   weatherCondition.innerHTML = `${condition} - ${temp}`;
   // var max = `${data.main.temp_max}&deg;`;
@@ -97,49 +141,24 @@ function displayData(data) {
   windSpeed.innerHTML = `${data.wind.speed}km/h`;
   visibility.innerHTML = `${(data.visibility / 1000).toFixed(2)}km`;
 
-  if (weather === "clear" || weather === "clear sky") {
+  if (weather === "Clear" || weather === "clear sky") {
     body.style.backgroundImage = "url(./imgs/background/clear.jpg)";
+    weatherIcon.src = "./imgs/icons/sun.png";
   } else if (
     weather === "few clouds" ||
-    weather === "cloudy" ||
-    weather === "overcast" ||
-    weather === "scattered clouds"
+    weather === "Clouds" ||
+    weather === "overcast clouds" ||
+    weather === "scattered clouds" ||
+    weather === "broken clouds"
   ) {
     body.style.backgroundImage = "url(./imgs/background/cloudy.jpg)";
-  } else if (weather === "rain" || weather === "light rain") {
-    body.style.backgroundImage = "url(./imgs/background/rainy.jpg";
-  } else if (weather === "haze") {
-    body.style.backgroundImage = "url(./imgs/background/haze.jpg";
+    weatherIcon.src = "./imgs/icons/cloudy.png";
+  } else if (weather === "Rain" || weather === "light rain") {
+    body.style.backgroundImage = "url(./imgs/background/rainy.jpg)";
+    weatherIcon.src = "./imgs/icons/rainy.png";
+  } else if (weather === "Haze" || weather === "Smoke") {
+    body.style.backgroundImage = "url(./imgs/background/haze.jpg)";
+  } else if (weather === "Snow") {
+    body.style.backgroundImage = "url(./imgs/background/snow.webp)";
   }
 }
-
-// function getData() {
-//   return new Promise(function (resolve, reject) {
-//     fetch(
-//       "https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=6be255ff5e9fc9c77fa08bd4064fc2a8&units=metric"
-//     )
-//       .then(function (res) {
-//         return res.json();
-//       })
-//       .then(function (res) {
-//         resolve(res);
-//       })
-//       .catch(function (res) {
-//         reject(res);
-//       });
-//   });
-// }
-
-// getData()
-//   .then(function (data) {
-//     temperature.innerHTML = data.main.temp;
-//   })
-//   .catch(function (data) {
-//     console.log("data", data);
-//   });
-
-// const heading = document.getElementById("heading");
-
-// setInterval(function () {
-//   heading.innerHTML = JSON.stringify(data);
-// }, 3000);
